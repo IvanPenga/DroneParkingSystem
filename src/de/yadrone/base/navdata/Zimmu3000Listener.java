c249761081d162bc8e8775fb49eec11f5f971074
Ivan Penga <ivapenga@foi.hr>
2017-06-24 13:56:08 +0200

    Projekt je ispravno preimenovan, a yadrone paketi su pospremljeni u jar

--- src/de/yadrone/base/navdata/Zimmu3000Listener.java
+++
@@ -1,9 +0,0 @@
-package de.yadrone.base.navdata;
-
-import java.util.EventListener;
-
-public interface Zimmu3000Listener extends EventListener {
-
-	public void location(double latitude, double longitude, double elevation);
-
-}
